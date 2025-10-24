91fa6302f043aaf0f0246e97a6ed2b7af657747c
bbbp98 <qjarms1300@gmail.com>
2025-10-24 18:03:29 +0900

    feat: camera move

---
+++ Assets/01. Scripts/Camera/CameraBoundsSetter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraBoundsSetter : MonoBehaviour
+{
+    public CameraFollow cameraFollow;
+    public Tilemap tilemap;
+
+    private void Start()
+    {
+        if (cameraFollow == null || tilemap == null)
+            return;
+
+        float halfHeight = Camera.main.orthographicSize;
+        float halfWidth = halfHeight * Camera.main.aspect;
+
+        Vector3 offSet = new Vector3(halfWidth, halfHeight);
+        Bounds bounds = tilemap.localBounds;
+        cameraFollow.minPosition = bounds.min + offSet;
+        cameraFollow.maxposition = bounds.max - offSet;
+        cameraFollow.maxposition.y -= tilemap.cellSize.y * 0.75f;
+
+        Debug.Log(cameraFollow.maxposition);
+    }
+}
---
+++ Assets/01. Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    [NonSerialized] public Vector2 minPosition;
+    [NonSerialized] public Vector2 maxposition;
+
+    [SerializeField] private float smoothSpeed = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        if (target == null)
+            return;
+
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        Vector3 pos = transform.position;
+        pos = target.position + offset;
+        float clampX = Mathf.Clamp(pos.x, minPosition.x, maxposition.x);
+        float clampY = Mathf.Clamp(pos.y, minPosition.y, maxposition.y);
+
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, pos, ref velocity, smoothSpeed);
+
+        transform.position = new Vector3(clampX, clampY, smoothPos.z);
+    }
+}
